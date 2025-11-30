9796793e0cc54c9f0fc2757a7b866ba2d1a9ff9a
Azbot-git <l.cohen@myskolae.fr>
2025-11-30 18:24:32 +0100

    Ajout du SDL et merge avec la branch Im-Savannah avec en plus l'ajout de la possibilité de se déplacé horrizontalement avec les fleche dirrectionnel

--- main.c
+++ main.c
@@ -1,191 +1,247 @@
 #include <stdio.h>
 #include <math.h>
+#include <SDL2/SDL.h>
 
+#define MAX_MASSES     16
+#define MAX_RESSORTS   32
 
-#define MAX_MASSES     32 // plus tard fair en sorte que se soit dynamique 1/2 stack
-#define MAX_RESSORTS   64 // 1 stack
+#define GRAVITY        1.0f
+#define SOL_Y          500.0f
 
-typedef struct {//structure des masses de la créature
+#define FORCE_DEPLACEMENT  8.0f
+
+#define ENERGIE_DEPART     100.0f
+#define ENERGIE_PERTE_SOL  0.1f
+#define ENERGIE_PERTE_TPS  0.003f
+
+typedef struct {
     float x, y;
     float vitessex, vitessey;
     float forcex, forcey;
-    float poid; 
+    float poid;
     float rayon;
     float amortissement;
-    int type_materiau; // id du materiaux
+    int type_materiau;
 } Masse;
 
-typedef struct{ // pemet de fair des precept de materiaux
+typedef struct {
     int id_materiaux;
     float poid;
     float rayon;
     float amortissement;
-}Materiaux;
+} Materiaux;
 
-typedef struct { //Structure des ressorts et des muscle ressorts
-    int masse1, masse2;            
-    float longueur;      
-    float rigidite;               
-    float amortissement;           
-    // Paramètre Ressort musculaires
-    int muscle; //1 est un muscle 0 n'est pas un muscle   
-    float amplitude;     
-    float frequence;            
+typedef struct {
+    int masse1, masse2;
+    float longueur;
+    float rigidite;
+    float amortissement;
+    int muscle;
+    float amplitude;
+    float frequence;
 } Ressort;
 
 typedef struct {
-    int id;
     Masse masses[MAX_MASSES];
     int nb_masses;
     Ressort ressorts[MAX_RESSORTS];
     int nb_ressorts;
-
     float energie;
     float temps_survie;
-    float distance_parcourue;
 } Creature;
 
-Materiaux listes_materiaux[4] = { //Listes des matériaux possible pour le joueur
-    { 0, 0.5f, 6.0f, 0.8f },  // chair
-    { 1, 1.0f, 7.0f, 0.3f },  // os
-    { 2, 0.3f, 8.0f, 1.3f },  // slime 
-    { 3, 1.8f, 5.0f, 0.1f }   // acier galvanisé
+Materiaux listes_materiaux[4] = {
+    { 0, 0.5f, 10.0f, 0.8f }, // chair
+    { 1, 1.0f, 12.0f, 0.3f }, // os
+    { 2, 0.3f, 8.0f, 1.3f },  // slime
+    { 3, 1.8f, 10.0f, 0.1f }  // acier
 };
 
-void Initialisation_creature(Creature *creature){// Initialisation de la creature avec les parametre par défaut
-    (*creature).nb_masses = 0;
-    (*creature).nb_ressorts = 0;
-    (*creature).energie = 100 ;
-    (*creature).temps_survie = 0;
-    (*creature).distance_parcourue = 0;
+void Initialisation_creature(Creature *c) {
+    c->nb_masses = 0;
+    c->nb_ressorts = 0;
+    c->energie = ENERGIE_DEPART;
+    c->temps_survie = 0;
 }
 
-void creature_ajoute_masse(Creature *creature,int x, int y,Materiaux type){
-    if ((*creature).nb_masses >= MAX_MASSES) {
-        printf("erreur trop de masses\n");
-        return;
-    }
-    Masse *masse = &creature->masses[(*creature).nb_masses];
-    (*creature).nb_masses += 1;
-
-    (*masse).x = x;
-    (*masse).y = y;
-    (*masse).vitessex = 0;
-    (*masse).vitessey = 0;
-    (*masse).forcex = 0;
-    (*masse).forcey = 0;
-    (*masse).poid = type.poid;
-    (*masse).rayon = type.rayon;
-    (*masse).amortissement = type.amortissement;
-    (*masse).type_materiau = type.id_materiaux;
+void creature_ajoute_masse(Creature *c, float x, float y, Materiaux type) {
+    Masse *m = &c->masses[c->nb_masses++];
+    m->x = x; m->y = y;
+    m->vitessex = m->vitessey = 0;
+    m->forcex = m->forcey = 0;
+    m->poid = type.poid;
+    m->rayon = type.rayon;
+    m->amortissement = type.amortissement;
+    m->type_materiau = type.id_materiaux;
 }
 
-void creature_ajoute_ressort(Creature *creature,int masse1,int masse2,float rigidite,float amortissement,int muscle,float amplitude,float frequence){
-    if ((*creature).nb_ressorts >= MAX_RESSORTS) { // creation d'un ressort entre deux masses en fonction des critere de la fonction
-        printf("erreur trop de muscle\n");
-        return;
-    }
+void creature_ajoute_ressort(Creature *c, int m1, int m2,
+                             float rigidite, float amortissement,
+                             int muscle, float amplitude, float frequence) {
+    Masse *a = &c->masses[m1];
+    Masse *b = &c->masses[m2];
+    float dx = b->x - a->x;
+    float dy = b->y - a->y;
+    float dist = sqrtf(dx*dx + dy*dy);
 
-    if (masse1 < 0 || masse1 >= creature->nb_masses || masse2 < 0 || masse2 >= creature->nb_masses) {
-        printf("erreur indices de masses invalides (%d, %d)\n", masse1, masse2);
-        return;
-    }
-
-    float dx = (*creature).masses[masse2].x - (*creature).masses[masse1].x;
-    float dy = (*creature).masses[masse2].y - (*creature).masses[masse1].y;
-    float distance = sqrtf(dx*dx + dy*dy);  
-
-
-    Ressort *ressort = &creature->ressorts[(*creature).nb_ressorts];
-    (*creature).nb_ressorts +=1;
-
-    (*ressort).masse1 = masse1;
-    (*ressort).masse2 = masse2;
-    (*ressort).longueur = distance;
-    (*ressort).rigidite = rigidite;
-    (*ressort).amortissement = amortissement;
-
-    (*ressort).muscle = muscle;
-    (*ressort).amplitude = amplitude;
-    (*ressort).frequence = frequence;
-
+    Ressort *r = &c->ressorts[c->nb_ressorts++];
+    r->masse1 = m1; r->masse2 = m2;
+    r->longueur = dist;
+    r->rigidite = rigidite;
+    r->amortissement = amortissement;
+    r->muscle = muscle;
+    r->amplitude = amplitude;
+    r->frequence = frequence;
 }
 
-void creature_appliquer_force(Creature *creature, float temps_total) {
-    for (int i = 0; i < (*creature).nb_masses; i++) { //aplique sur chaque masse la gravité de base
+void draw_circle(SDL_Renderer *renderer, int x, int y, int rayon) {
+    for (int w = 0; w < rayon * 2; w++)
+        for (int h = 0; h < rayon * 2; h++) {
+            int dx = rayon - w;
+            int dy = rayon - h;
+            if (dx*dx + dy*dy <= rayon*rayon)
+                SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+        }
+}
+
+void set_color_material(SDL_Renderer *r, int type) {
+    switch(type) {
+        case 0: SDL_SetRenderDrawColor(r, 220,50,50,255); break;   // chair rouge
+        case 1: SDL_SetRenderDrawColor(r, 240,240,240,255); break; // os blanc
+        case 2: SDL_SetRenderDrawColor(r, 80,220,80,255); break;   // slime vert
+        case 3: SDL_SetRenderDrawColor(r, 50,100,220,255); break;  // acier bleu
+    }
+}
+
+void creature_appliquer_force(Creature *creature, float temps,int input_gauche, int input_droite) {
+    float force_horizontale = 0;
+    if (input_gauche) {
+        force_horizontale -= FORCE_DEPLACEMENT;
+    }
+    if (input_droite) {
+        force_horizontale += FORCE_DEPLACEMENT;
+    }
+    for (int i = 0; i < creature->nb_masses; i++) {
         Masse *masse = &creature->masses[i];
-        (*masse).forcex = 0;
-        (*masse).forcey = (*masse).poid * 9.81f; // 9.81 gravité arrondie
-    }
-
-    for (int i = 0; i < creature->nb_ressorts; i++) { //Pour chaque ressort aplique les forces celon le type de liasion muscle etc
-
-        Ressort *ressort = &creature->ressorts[i];
-
-        Masse *masse1 = &creature->masses[ressort->masse1];
-        Masse *masse2 = &creature->masses[ressort->masse2];
-
-        float distancex = masse2->x - masse1->x;
-        float distancey = masse2->y - masse1->y;
-        float distance = sqrtf(distancex*distancex + distancey*distancey);
-
-        float longueur = ressort->longueur;
-
-        if (ressort->muscle) { //Pour que le muscle change la longueur en fonction du temp sur le temp 
-            float oscillation = ressort->amplitude * sinf(2.0f * 3.14159f * ressort->frequence * temps_total);// oscillation permet de savoir si un muscle s'etire en longueur donc  positif ou retractre négatif d'ou le sinf => sinus et PI
-            longueur = ressort->longueur * (1.0f + oscillation); //si besoin video explicative "https://www.youtube.com/watch?v=0fGSabpRA-8"
+        
+        if (masse->y >= SOL_Y) { //Deplacement uniquement si la masse touche le sol sinon on se deplace pas dans les air en gros sans appuie
+            masse->forcex = force_horizontale; // ajoute le deplacment a la masse en question
+        } else {
+            masse->forcex = 0;
         }
-
-        float force = ressort->rigidite * (distance - longueur);
-        float newx = distancex / distance;
-        float newy = distancey / distance;
-
-        float distancevitessex = masse2->vitessex - masse1->vitessex;
-        float distancevitessey = masse2->vitessey - masse1->vitessey;
-        float amortissement = ressort->amortissement * (distancevitessex * newx + distancevitessey * newy);
-
-        float forcex = (force + amortissement) * newx;
-        float forcey = (force + amortissement) * newy;
-
-        //Aplication des force opposé
-        masse1->forcex += forcex;
-        masse1->forcey += forcey;
-
-        masse2->forcex -= forcex;
-        masse2->forcey -= forcey;
+        masse->forcey = masse->poid * GRAVITY;
+        if (masse->y > SOL_Y) {
+            masse->y = SOL_Y;
+            masse->vitessey = 0;        // le sol arrete le mouvement vertical
+            creature->energie -= ENERGIE_PERTE_SOL;
+        }
     }
 }
 
-int main(void) {
-    Creature creature;
-    Initialisation_creature(&creature);
-
-    creature_ajoute_masse(&creature, 100.0f, 200.0f, listes_materiaux[0]);
-    creature_ajoute_masse(&creature, 140.0f, 200.0f, listes_materiaux[1]);
-    creature_ajoute_ressort(&creature, 0, 1, 50.0f, 0.5f, 1, 0.2f, 2.0f);
-
-    float temps = 0.0f;
-    float deltatemp = 0.016f; // 60 FPS (= combien de frame/image par seconde actuellement c'est 1/60 )
-
-    for (int step = 0; step < 10; ++step) {
-        creature_appliquer_force(&creature, temps);
-
-        for (int i = 0; i < creature.nb_masses; ++i) {
-            Masse *masse = &creature.masses[i];
-            float ax = masse->forcex / masse->poid;
-            float ay = masse->forcey / masse->poid;
-            masse->vitessex += ax * deltatemp;
-            masse->vitessey += ay * deltatemp;
-            masse->x += masse->vitessex * deltatemp;
-            masse->y += masse->vitessey * deltatemp;
-        }
-        printf("distance x : %2f Distance y : %2f \n",creature.masses[0].x,creature.masses[0].y);
-
-        temps += deltatemp;
-    }
-
-    printf("Simulation terminée\n");
-    return 0;
+void afficher_HUD(SDL_Renderer *r, Creature *c) {
+    SDL_SetRenderDrawColor(r, 40, 40, 40, 200);
+    SDL_Rect hud = {10, 10, 200, 40};
+    SDL_RenderFillRect(r, &hud);
 }
 
-//video expliquant le systeme masse ressort "https://www.youtube.com/watch?v=qMx4GSX6-2s" si besoin
+int main(int argc, char *argv[]) {
+    Creature c;
+    Initialisation_creature(&c);
+
+    int input_gauche = 0; // Deplacement 
+    int input_droite = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("Creature Masse-Ressort",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, 0);
+    SDL_Renderer *r = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    creature_ajoute_masse(&c, 400,150, listes_materiaux[1]); // tête (os)
+    creature_ajoute_masse(&c, 400,200, listes_materiaux[0]); // torse (chair)
+    creature_ajoute_masse(&c, 360,200, listes_materiaux[0]); // bras G
+    creature_ajoute_masse(&c, 440,200, listes_materiaux[0]); // bras D
+    creature_ajoute_masse(&c, 380,260, listes_materiaux[2]); // jambe G (slime)
+    creature_ajoute_masse(&c, 420,260, listes_materiaux[2]); // jambe D
+
+    creature_ajoute_ressort(&c, 0,1, 30,1.0f, 1,0.03f,1); // tête-torse
+    creature_ajoute_ressort(&c, 1,2, 35,1.0f, 0,0,0);     // torse-bras G
+    creature_ajoute_ressort(&c, 1,3, 35,1.0f, 0,0,0);     // torse-bras D
+    creature_ajoute_ressort(&c, 1,4, 40,1.0f, 1,0.05f,1); // torse-jambe G
+    creature_ajoute_ressort(&c, 1,5, 40,1.0f, 1,0.05f,1); // torse-jambe D
+
+    float temps = 0.0f, dt = 0.016f;
+    int running = 1;
+
+    while (running) {
+        if (c.energie <= 0) break;
+
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = 0;
+
+            if (e.type == SDL_KEYDOWN) { //presse vert le bas
+                if (e.key.keysym.sym == SDLK_LEFT) {
+                    input_gauche = 1;
+                } else if (e.key.keysym.sym == SDLK_RIGHT) {
+                    input_droite = 1;
+                }
+            } else if (e.type == SDL_KEYUP) { // relache le bouton
+                if (e.key.keysym.sym == SDLK_LEFT) {
+                    input_gauche = 0;
+                } else if (e.key.keysym.sym == SDLK_RIGHT) {
+                    input_droite = 0;
+                }
+        }
+    }
+        creature_appliquer_force(&c, temps,input_gauche, input_droite);
+        c.temps_survie += dt;
+        c.energie -= ENERGIE_PERTE_TPS;
+
+        // intègre les forces → mouvement
+        for (int i = 0; i < c.nb_masses; i++) {
+            Masse *m = &c.masses[i];
+            m->vitessex += (m->forcex / m->poid) * dt;
+            m->vitessey += (m->forcey / m->poid) * dt;
+            m->vitessex *= 0.98f;
+            m->vitessey *= 0.98f;
+            m->x += m->vitessex * dt;
+            m->y += m->vitessey * dt;
+        }
+
+        SDL_SetRenderDrawColor(r, 20,20,20,255);
+        SDL_RenderClear(r);
+
+        // Sol
+        SDL_SetRenderDrawColor(r, 120,120,120,255);
+        SDL_Rect sol = {0, (int)SOL_Y + 10, 800, 20};
+        SDL_RenderFillRect(r, &sol);
+
+        // Ressorts
+        SDL_SetRenderDrawColor(r, 255,255,255,255);
+        for(int i=0;i<c.nb_ressorts;i++){
+            Ressort *rs=&c.ressorts[i];
+            Masse *m1=&c.masses[rs->masse1];
+            Masse *m2=&c.masses[rs->masse2];
+            SDL_RenderDrawLine(r,m1->x,m1->y,m2->x,m2->y);
+        }
+
+        // Masses (cercles)
+        for(int i=0;i<c.nb_masses;i++){
+            Masse *m = &c.masses[i];
+            set_color_material(r, m->type_materiau);
+            draw_circle(r, m->x, m->y, m->rayon);
+        }
+
+        afficher_HUD(r, &c);
+
+        SDL_RenderPresent(r);
+        temps += dt;
+    }
+
+    SDL_DestroyRenderer(r);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    exit(EXIT_SUCCESS);
+}
+
